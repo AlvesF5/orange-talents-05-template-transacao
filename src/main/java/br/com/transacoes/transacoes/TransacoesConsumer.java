1bda16d8fb650bb402ffdff694a6ff92a657c0ea
AlvesF5 <matheus.cruz_@hotmail.com>
2021-06-24 19:34:43 -0300

    Remoção de função topic da classe TransacoesConsumer

--- src/main/java/br/com/transacoes/transacoes/TransacoesConsumer.java
+++ src/main/java/br/com/transacoes/transacoes/TransacoesConsumer.java
@@ -1,10 +1,10 @@
 package br.com.transacoes.transacoes;
 
-import org.apache.kafka.clients.admin.NewTopic;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
+
 import org.springframework.stereotype.Component;
 
 import br.com.transacoes.transacoes.domain.Transacao;
@@ -15,13 +15,6 @@ public class TransacoesConsumer {
 	
 	@Autowired
 	private TransacaoRepository transacaoRepository;
-	
-	
-	@Bean
-	public NewTopic topic() {
-		return TopicBuilder.name("transacoes").partitions(1).replicas(1).build();
-	}
-
 
 
 	@KafkaListener(id = "transacoes", topics = "transacoes")
