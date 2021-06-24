5a098eefe3a0ca5eb29f691ce649f36ef17d9ab5
AlvesF5 <matheus.cruz_@hotmail.com>
2021-06-24 17:06:25 -0300

    Consutando transações - ordenação por data

--- src/main/java/br/com/transacoes/transacoes/controller/TransacaoController.java
+++ src/main/java/br/com/transacoes/transacoes/controller/TransacaoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +33,9 @@ public class TransacaoController {
 	private CartaoRepository cartaoRepository;
 	
 	@GetMapping("/{idCartao}")
-	public ResponseEntity<Page<TransacaoResponse>>  transacoes(@PathVariable("idCartao") String idCartao, @PageableDefault(page = 0, size = 10) Pageable paginacao){
+	public ResponseEntity<Page<TransacaoResponse>>  transacoes(@PathVariable("idCartao") String idCartao, 
+			@PageableDefault(sort =  "efetivadaEm", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao){
+		
 		
 		Optional<Cartao> possivelCartao = cartaoRepository.findById(idCartao);
 		
